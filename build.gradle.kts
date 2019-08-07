1870fbe59b6d11d4f7dbb439d236aea7ab1a7dee
Alexander Weickmann <alexander.weickmann@link-time.com>
2019-08-07 15:38:46 +0200

    set version 1.1.0-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 println("Gradle Version: " + GradleVersion.current().toString())
 
 group = "com.link-time.ktor"
-version = "1.0.0"
+version = "1.1.0-SNAPSHOT"
 
 val kotlinVersion = "1.3.50-eap-54"
 val ktorVersion = "1.1.3"
