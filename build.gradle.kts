94eead938a5c2835036e89937b952e2880e10b2c
Patrick Louis <patrick.louis@link-time.com>
2022-11-18 08:06:33 +0100

    Updated dependencies & gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
-import groovy.lang.Closure
 import nu.studer.gradle.credentials.domain.CredentialsContainer
-import org.gradle.util.GradleVersion
 import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -8,35 +6,30 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 println("Gradle Version: " + GradleVersion.current().toString())
 
 group = "com.linked-planet.ktor"
-version = "1.2.1-ktor-1.4.2"
+version = "1.2.1-ktor-2.1.3"
 
-val kotlinVersion = "1.4.0"
-val ktorVersion = "1.4.2"
+val kotlinVersion = "1.7.21"
+val ktorVersion = "2.1.3"
 val jvmTarget = "1.8"
 
 plugins {
-    kotlin("jvm") version "1.3.72"
+    kotlin("jvm") version "1.7.21"
     id("org.jetbrains.dokka") version "0.10.1"
     id("com.github.ben-manes.versions") version "0.28.0"
-    id("com.bmuschko.nexus") version "2.3.1"
-    id("io.codearte.nexus-staging") version "0.21.2"
     id("nu.studer.credentials") version "2.1"
     `maven-publish`
 }
 
 repositories {
     mavenCentral()
-    jcenter()
-    maven { url = uri("http://dl.bintray.com/kotlin/ktor") }
-    maven { url = uri("http://dl.bintray.com/kotlin/kotlin-eap") }
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8", version = kotlinVersion))
     api(group = "io.ktor", name = "ktor-server-jetty", version = ktorVersion)
-    api(group = "io.ktor", name = "ktor-locations", version = ktorVersion)
-    api(group = "io.ktor", name = "ktor-html-builder", version = ktorVersion)
-    api(group = "com.onelogin", name = "java-saml", version = "2.4.0")
+    api(group = "io.ktor", name = "ktor-server-locations", version = ktorVersion)
+    api(group = "io.ktor", name = "ktor-server-html-builder", version = ktorVersion)
+    api(group = "com.onelogin", name = "java-saml", version = "2.9.0")
 }
 
 tasks.withType<KotlinCompile> {
@@ -47,8 +40,38 @@ tasks.withType<KotlinCompile> {
 // ability to publish artifact to maven local
 publishing {
     publications {
-        register("mavenJava", MavenPublication::class.java) {
+        create<MavenPublication>("ktor-onelogin-saml") {
             from(components["kotlin"])
+            pom {
+                name.set("Ktor OneLogin SAML Integration")
+                description.set("Integrates Ktor with OneLogin java-saml library.")
+                url.set("https://github.com/linked-planet/ktor-onelogin-saml")
+                inceptionYear.set("2019")
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("Alexander Weickmann")
+                        email.set("alexander.weickmann@gmail.com")
+                        url.set("https://github.com/weickmanna")
+                        organization.set("linked-planet GmbH")
+                        organizationUrl.set("https://linked-planet.com")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/linked-planet/ktor-onelogin-saml.git")
+                    connection.set("scm:git:git://github.com/linked-planet/ktor-onelogin-saml.git")
+                    developerConnection.set("scm:git:git://github.com/linked-planet/ktor-onelogin-saml.git")
+                }
+            }
+            repositories {
+                mavenCentral()
+            }
         }
     }
 }
@@ -69,53 +92,6 @@ gradle.taskGraph.whenReady {
             extra["nexusPassword"] = credentials.getProperty("nexusToken")
         }
     }
-}
-
-// add pom metadata needed for publishing to maven central
-val modifyPom: Closure<MavenPom> by ext
-modifyPom(closureOf<MavenPom> {
-    project {
-        withGroovyBuilder {
-            "name"("Ktor OneLogin SAML Integration")
-            "description"("Integrates Ktor with OneLogin java-saml library.")
-            "url"("https://github.com/linked-planet/ktor-onelogin-saml")
-            "inceptionYear"("2019")
-            "licenses" {
-                "license" {
-                    "name"("The Apache Software License, Version 2.0")
-                    "url"("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    "distribution"("repo")
-                }
-            }
-            "developers" {
-                "developer" {
-                    "name"("Alexander Weickmann")
-                    "email"("alexander.weickmann@gmail.com")
-                    "url"("https://github.com/weickmanna")
-                    "organization"("linked-planet GmbH")
-                    "organizationUrl"("https://linked-planet.com")
-                }
-            }
-            "scm" {
-                "url"("https://github.com/linked-planet/ktor-onelogin-saml.git")
-                "connection"("scm:git:git://github.com/linked-planet/ktor-onelogin-saml.git")
-                "developerConnection"("scm:git:git://github.com/linked-planet/ktor-onelogin-saml.git")
-            }
-        }
-    }
-})
-
-// disable archives set by nexus plugin, since we use Kotlin we have to create our own ones
-extraArchive {
-    sources = false
-    tests = false
-    javadoc = false
-}
-
-nexusStaging {
-    stagingProfileId = "21e9ad4c172f4"
-    delayBetweenRetriesInMillis = 12000
-    numberOfRetries = 30
 }
 
 tasks {
