cbfaf0fbf0b3462abadb0db144e69394d45f44dd
Alexander Weickmann <alexander.weickmann@linked-planet.com>
2023-02-06 17:24:59 +0100

    update ktor to 2.2.3, kotlin, java etc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,34 @@
-import org.jetbrains.dokka.gradle.DokkaTask
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-// print gradle version as we might run on different machines as well as in the cloud
 println("Gradle Version: " + GradleVersion.current().toString())
 
-val libraryVersion = scmVersion.version
 group = "com.linked-planet"
-version = libraryVersion
+version = scmVersion.version
 
-val kotlinVersion = "1.7.21"
-val ktorVersion = "2.1.3"
-val jvmTarget = "1.8"
+val kotlinVersion = "1.8.10"
+val ktorVersion = "2.2.3"
 
 plugins {
-    kotlin("jvm") version "1.7.21"
-    id("org.jetbrains.dokka") version "0.10.1"
-    id("com.github.ben-manes.versions") version "0.28.0"
-    id("nu.studer.credentials") version "2.1"
-    id("pl.allegro.tech.build.axion-release") version "1.14.2"
+    kotlin("jvm") version "1.8.10"
+
+    // misc
+    id("nu.studer.credentials") version "3.0"
+
+    // derive gradle version from git tag
+    id("pl.allegro.tech.build.axion-release") version "1.14.3"
+
+    // publishing
     id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
     id("signing")
     `maven-publish`
+
+    // provide & configure dependencyUpdates
+    id("com.github.ben-manes.versions") version "0.45.0"
+    id("se.ascp.gradle.gradle-versions-filter") version "0.1.16"
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
 repositories {
@@ -28,15 +36,15 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8", version = kotlinVersion))
-    api(group = "io.ktor", name = "ktor-server-jetty", version = ktorVersion)
-    api(group = "io.ktor", name = "ktor-server-locations", version = ktorVersion)
-    api(group = "io.ktor", name = "ktor-server-html-builder", version = ktorVersion)
-    api(group = "com.onelogin", name = "java-saml", version = "2.9.0")
+    implementation(kotlin("stdlib-jdk8", kotlinVersion))
+    api("io.ktor", "ktor-server-jetty", ktorVersion)
+    api("io.ktor", "ktor-server-locations", ktorVersion)
+    api("io.ktor", "ktor-server-html-builder", ktorVersion)
+    api("com.onelogin", "java-saml", "2.9.0")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = jvmTarget
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
     kotlinOptions.freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
 }
 
@@ -51,13 +59,7 @@ signing {
 
 
 tasks {
-//    register("dokkaJavadoc", DokkaTask::class) {
-//        outputFormat = "javadoc"
-//        outputDirectory = "$buildDir/javadoc"
-//        configuration.reportUndocumented = false
-//    }
     register("javadocJar", Jar::class) {
-//        dependsOn("dokkaJavadoc")
         archiveClassifier.set("javadoc")
         from("$buildDir/javadoc")
     }
